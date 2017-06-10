116af8d7bc50c0bfdc076ed5ee7fa3a7bb4b22b0
gyunxiu <gyunxiu@gmail.com>
2017-06-09 17:37:55 -0700

    12) Search Engine Optimize Your Site

--- header.php
+++ header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
+        <title><?php get_my_title_tag(); ?></title>
         <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
+        <meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>" />
 
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
         <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nav.css" type="text/css" media="all" />
